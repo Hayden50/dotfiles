025bb7890ea147b56fd9229f1356990f108a4704
Hayden50 <haydenrieder50@hotmail.com>
2024-04-30 18:37:15 -0400

    Add NVIM plugins and update colorscheme

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,1 +1,2 @@
 require("hayden.core")
+require("hayden.lazy")
--- nvim/lua/hayden/core/keymaps.lua
+++ nvim/lua/hayden/core/keymaps.lua
@@ -7,11 +7,13 @@ keymap.set("i", "<leader>jk", "<ESC>", {desc = "Exit insert mode with jk"})
 -- Normal Mode 
 keymap.set('n', 'x', '"_x') -- deletes a single character w/o adding to reg
 keymap.set('n', '<cr>', 'ciw', {desc = "Deletes current work and enters i mode "})
-keymap.set('n', '<Tab>', '<C-W>w'); -- jumps between windows 
-keymap.set('n', '<S-Tab>', '<C-W>W');
-keymap.set('n', '<leader><Tab>', ':tabnext<CR>'); -- jumps between tabs 
-keymap.set('n', '<leader><S-Tab>', ':tabprevious<CR>');
+keymap.set('n', '<leader><Tab>', '<C-W>w'); -- jumps between windows 
+keymap.set('n', '<leader><S-Tab>', '<C-W>W');
+keymap.set('n', '<Tab>', ':tabnext<CR>'); -- jumps between tabs 
+keymap.set('n', '<S-Tab>', ':tabprevious<CR>');
 keymap.set('n', '<leader><leader>', ':tabnew<CR>'); -- creates a new tab 
+keymap.set('n', '<leader>|', ':vsplit<CR>') -- creates a vertical window
+keymap.set('n', '<leader>-', ':split<CR>') -- creates a horizontal window
      
 -- Visual Mode 
 keymap.set("v", "y", "ygv<esc>")   
